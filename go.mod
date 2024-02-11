c33d55c95b8fd6343231acc724939c04ebcbc1fe
Evan Dolatowski <evandolatowski@yahoo.com>
2024-02-11 11:58:10 -0600

    testing godotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GnarlyLasagna/go-blog-aggregator
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+)
 
 func main(){
 
+    godotenv.Load(".env")
+
     fmt.Println("hello world")
+
+    portString := os.Getenv("PORT")
+    if portString == ""{
+        log.Fatal("PORT is not found in the env file")
+    }
+
+    fmt.Println("PORT:", portString)
 }
