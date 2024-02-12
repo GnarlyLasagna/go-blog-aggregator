cae21c1cb2aa2118e9fec141000ea0d33a37e264
Evan Dolatowski <evandolatowski@yahoo.com>
2024-02-12 12:04:07 -0600

    adding sql db

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
     "os"
     "net/http"
 
+    _ "github.com/lib/pq"
     "github.com/joho/godotenv"
     "github.com/go-chi/chi/v5"
     "github.com/go-chi/cors"
+    "github.com/GnarlyLasagna/go-blog-aggregator/internal/database"
 )
 
 
@@ -20,6 +22,23 @@ func main() {
 	if portString == "" {
 		log.Fatal("Port not found")
 	}
+
+    dbURL := os.Getenv("DB_URL")
+	if DbURL == "" {
+		log.Fatal("Database URL is not found in the environment")
+	}
+
+    conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	db := database.New(conn)
+	apiCfg := apiConfig{
+		DB: db,
+	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -36,6 +55,7 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+    v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)	
 
